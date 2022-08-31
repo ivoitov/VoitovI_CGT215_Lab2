99f1fc7eb0953b01ca0dd45b64f5ce36fb918df0
ivoitov <ivoitov@purdue.edu>
2022-08-31 12:14:21 -0400

    CGT 215 Lab 2

--- VoitovI_CGT215_Lab2/VoitovI_CGT215_Lab2.cpp
+++ VoitovI_CGT215_Lab2/VoitovI_CGT215_Lab2.cpp
@@ -1,20 +1,26 @@
-// VoitovI_CGT215_Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+	cout << "Hi, I'm Ivan and today I'm going to solve the equation:\n";
+	cout << "Ax + B = 0\n";
+	cout << "For x\n";
+	cout << "\n";
+	cout << "Please enter a value for A:";
+	cin >> A;
+	cout << "Please enter a value for B:";
+	cin >> B;
+	cout << "\n";
+	cout << "Solving ";
+	cout << A;
+	cout << "x+";
+	cout << B;
+	cout << "=0 for x...\n";
+	X = (B*(-1))/A;
+	cout << "\n";
+	cout << "The solution for x is:\n";
+	cout << "x=" << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
